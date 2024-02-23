d04e98994768269889a136cf2811fa6df96da8f3
whichghost <whichghost@proton.me>
2024-02-23 12:31:04 +0800

    implement root info

--- service/InfoService.js
+++ service/InfoService.js
@@ -1,5 +1,6 @@
 'use strict';
-
+var fs = require('fs'), path = require('path');
+var jsyaml = require('js-yaml');
 
 /**
  * API Info
@@ -8,14 +9,17 @@
  * returns List
  **/
 exports.info = function() {
+  var spec = fs.readFileSync(path.join(__dirname,'../api/swagger.yaml'), 'utf8');
+  var swaggerDoc = jsyaml.load(spec);
+
   return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = {};
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
+    var info = {
+      version: swaggerDoc.info.version,
+      date: Date.now(),
+      kubernetes: process.env.IS_KUBERNETES==='true'
+    };
+
+    resolve(info);
   });
 }
 
