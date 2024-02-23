a44d52917ee5b5fd34dc8f3775cabea5fe7e57f6
whichghost <whichghost@proton.me>
2024-02-23 12:16:25 +0800

    implement /health

--- service/HealthService.js
+++ service/HealthService.js
@@ -9,17 +9,7 @@
  **/
 exports.health = function() {
   return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = {
-  "blank": true,
-  "bytes": [],
-  "empty": true
-};
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
       resolve();
-    }
   });
 }
 
